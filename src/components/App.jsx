8b56b56ea176af960928717fc6314ec95c0a605f
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-06-21 09:58:59 +0100

    Initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="left-scene">
+        <input className="form-search" />
+        <div className="selected-gif">
+        THE SELECTED GIF HERE
+        </div>
+      </div>
+
+      <div className="right-scene">
+        THE SIDEBAR GIF LIST HERE
+      </div>
+    </div>
+  )
+}
+
+export default App;

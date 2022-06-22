aecd63e0ff9065d90cfe0729c984fe4389592208
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-06-22 10:53:35 +0100

    Gif selection is now working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import giphy from 'giphy-api';
 import './App.css';
 import Gif from './Gif';
-import GifList from './GifList';
 import Search from './Search';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [selectedId, setSelected] = useState();
+
+  useEffect(() => {
+    giphy(process.env.REACT_APP_GIPHY_API).search(
+      {
+        q: "pokemon",
+        rating: 'g',
+        limit: 10,
+      },
+      (_err, res) => {
+        setData(res.data);
+      },
+    );
+  }, []);
+
+  const handleSelect = ({id}) => {
+    setSelected(id);
+  }
+
   return (
     <div className="app">
       <div className="left-scene">
         <input className="form-search" />
-        <div className="selected-gif">THE SELECTED GIF HERE</div>
+        <div className="selected-gif">
+          <Gif id={selectedId}/>
+        </div>
       </div>
 
       <div className="right-scene">
-        <GifList searchText="pokemon" />
+        <div className="gif-list">
+          {data.map((gif) => {
+            console.log(gif);
+            return <Gif
+                    key={gif.id}
+                    id={gif.id}
+                    onSelect={() => handleSelect(gif)}
+                    />;
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,7 +1,7 @@
-const Gif = ({ id }) => {
+const Gif = ({ id, onSelect }) => {
   const url = `https://media2.giphy.com/media/${id}/200w.webp`;
 
-  return <img src={url} alt="Giphy square" />;
+  return <img src={url} alt="Giphy square" onClick={onSelect}/>;
 };
 
 export default Gif;
